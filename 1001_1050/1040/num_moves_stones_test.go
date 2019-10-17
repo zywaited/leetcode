22cc59d8ce7466dbbe5c508440b660c9486a8946
waited <zhangjian@medlinker.com>
2019-10-17 20:53:38 +0800

    add: 1033、1040 移动石子

--- 1001_1050/1040/num_moves_stones_test.go
+++ 1001_1050/1040/num_moves_stones_test.go
@@ -11,4 +11,5 @@ import (
 func TestNumMovesStones(t *testing.T) {
 	require.Equal(t, []int{1, 2}, one.NumMovesStonesII([]int{7, 4, 9}))
 	require.Equal(t, []int{2, 3}, one.NumMovesStonesII([]int{6, 5, 4, 3, 10}))
+	require.Equal(t, []int{0, 0}, one.NumMovesStonesII([]int{100, 101, 104, 102, 103}))
 }
--- 1001_1050/1040/one/num_moves_stonesII.go
+++ 1001_1050/1040/one/num_moves_stonesII.go
@@ -14,17 +14,18 @@ func NumMovesStonesII(stones []int) []int {
 	rs[1] = max(stones[sn-2]-stones[0], stones[sn-1]-stones[1]) - sn + 2
 	rs[0] = rs[1]
 	ms := 0 // 最小次数
-	i, j := 0, 0
+	i, j := 0, 1
 	for ; i < sn; i++ {
 		// 找到一个位置区间可以放下所有的石子
-		for j+1 < sn && stones[j+1]-stones[i]+1 <= sn {
+		// 这里用<=sn是因为==sn可能是刚好不用移动的情况，这时就是需要判断是否后面还有石子
+		for j < sn && stones[j]-stones[i]+1 <= sn {
 			j++
 		}
-		ms = sn - (j - i + 1)
+		ms = sn - (j - i)
 		// 但是给出的实例中[6,5,4,3,10]不能通过这种方法移动
 		// 这种情况最小值就是2
 		// 特殊处理
-		if ms == 1 && stones[j]-stones[i]+1 == sn-1 {
+		if ms == 1 && stones[j-1]-stones[i]+1 == sn-1 {
 			ms = 2
 		}
 		rs[0] = min(rs[0], ms)
