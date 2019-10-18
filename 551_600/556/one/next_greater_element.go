24b0b1b9d52502e29168f50411e2c2b6494cc55a
waited <zhangjian@medlinker.com>
2019-10-18 08:59:17 +0800

    add: 优化: 556

--- 551_600/556/one/next_greater_element.go
+++ 551_600/556/one/next_greater_element.go
@@ -6,19 +6,12 @@ import (
 )
 
 func NextGreaterElement(n int) int {
-	bits := make([]int, 0)
 	// 递减链表(双向)
 	origin := &list.List{}
-
 	bit := 0
 	find := false
-	for n != 0 {
+	for n != 0 && !find {
 		n, bit = n/10, n%10
-		if find {
-			bits = append(bits, bit)
-			continue
-		}
-
 		cn := origin.Front()
 		mn := (*list.Element)(nil)
 		for cn != nil && cn.Value.(int) > bit {
@@ -27,7 +20,7 @@ func NextGreaterElement(n int) int {
 		}
 		if mn != nil {
 			find = true
-			bits = append(bits, mn.Value.(int))
+			n = n*10 + mn.Value.(int)
 			mn.Value = bit
 			continue
 		}
@@ -35,19 +28,8 @@ func NextGreaterElement(n int) int {
 	}
 	rs := -1
 	if find {
-		index := 0
+		rs = n
 		prevMax, prevMaxBit := math.MaxInt32/10, math.MaxInt32%10
-		for index = len(bits) - 1; index >= 0; index-- {
-			if rs == -1 {
-				rs = bits[index]
-				continue
-			}
-			// 是否超过32位
-			if rs > prevMax || (rs == prevMax && bits[index] > prevMaxBit) {
-				return -1
-			}
-			rs = rs*10 + bits[index]
-		}
 		cn := origin.Back()
 		for cn != nil {
 			// 是否超过32位
