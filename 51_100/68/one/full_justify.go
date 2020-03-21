7f858edbd8ff7d5d5fe7962c2ce1669ac68dfb88
waited <zhangjian@medlinker.com>
2020-03-21 18:55:34 +0800

    add: 算法题68

--- 51_100/68/one/full_justify.go
+++ 51_100/68/one/full_justify.go
@@ -1,13 +1,11 @@
 package one
 
-import "strings"
-
 func FullJustify(words []string, maxWidth int) []string {
 	ans := make([]string, 0)
 	cl := 0  // 当前行的单词长度
 	cwl := 0 // 当前的单词个数
 	uel := 0 // 截止上一行使用的单词个数
-	cb := strings.Builder{}
+	cb := make([]byte, 0)
 	tmp := 0
 	for index, word := range words {
 		wl := len(word)
@@ -16,48 +14,48 @@ func FullJustify(words []string, maxWidth int) []string {
 			// 只有一个单词后面全是空格
 			if cwl == 1 {
 				for tmp = maxWidth - cl - cwl; tmp > 0; tmp-- {
-					cb.WriteString(" ")
+					cb = append(cb, ' ')
 				}
 			} else {
 				// 判断空格是否均分
 				bs, es := (maxWidth-cl)/(cwl-1), (maxWidth-cl)%(cwl-1)
-				cb.Reset()
+				cb = cb[:0]
 				for tmp = uel; tmp <= index-2; tmp++ {
-					cb.WriteString(words[tmp])
+					cb = append(cb, words[tmp]...)
 					if es > 0 {
-						cb.WriteString(" ")
+						cb = append(cb, ' ')
 						es--
 					}
 					for tbs := bs; tbs > 0; tbs-- {
-						cb.WriteString(" ")
+						cb = append(cb, ' ')
 					}
 				}
-				cb.WriteString(words[index-1]) // 写入最后的那个数据
+				cb = append(cb, words[index-1]...) // 写入最后的那个数据
 			}
 			uel += cwl
 			cl = 0
 			cwl = 0
-			ans = append(ans, cb.String())
-			cb.Reset()
+			ans = append(ans, string(cb))
+			cb = cb[:0]
 		}
-		cb.WriteString(word)
+		cb = append(cb, word...)
 		if cl+wl+cwl == maxWidth {
-			ans = append(ans, cb.String())
+			ans = append(ans, string(cb))
 			cl = 0
 			uel += cwl + 1
 			cwl = 0
-			cb.Reset()
+			cb = cb[:0]
 			continue
 		}
 		// 是否是最后一行
 		if index == len(words)-1 {
 			for tmp = maxWidth - wl - cl - cwl; tmp > 0; tmp-- {
-				cb.WriteString(" ")
+				cb = append(cb, ' ')
 			}
-			ans = append(ans, cb.String())
+			ans = append(ans, string(cb))
 			break // 结束了
 		}
-		cb.WriteString(" ")
+		cb = append(cb, ' ')
 		cl += wl
 		cwl++
 	}
