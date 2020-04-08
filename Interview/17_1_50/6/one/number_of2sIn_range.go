07a4febea3f8b24a05ca60eaf0f847498a56cd4f
waited <zhangjian@medlinker.com>
2020-04-08 13:37:50 +0800

    add: 面试题17.6

---
+++ Interview/17_1_50/6/one/number_of2sIn_range.go
@@ -0,0 +1,51 @@
+package one
+
+import "math"
+
+func NumberOf2sInRange(n int) int {
+	// 记录每个位置的数字
+	nn := make([]int, 0)
+	ans := 0
+	tn := n
+	pn := 0
+	for tn > 0 {
+		// 获取当前的数字
+		pn = tn % 10
+		tn /= 10
+		nn = append(nn, pn)
+		if tn == 0 {
+			break
+		}
+		// 这个时候位数还有多的
+		ans += int(math.Pow10(len(nn) - 1))
+		if len(nn)-2 >= 0 {
+			ans += 9 * int(math.Pow10(len(nn)-2)) * (len(nn) - 1)
+		}
+	}
+	// 依次变化2的位置
+	for i := len(nn) - 1; i >= 0; i-- {
+		if nn[i] > 2 {
+			ans += int(math.Pow10(i))
+		} else if nn[i] == 2 {
+			ans++
+			// == 2情况比较复杂，需要(i-1)-0都计算一次
+			for k := i - 1; k >= 0; k-- {
+				if nn[k] > 0 {
+					ans += nn[k] * int(math.Pow10(k))
+				}
+			}
+		}
+		for j := i + 1; j < len(nn); j++ {
+			if j == len(nn)-1 {
+				if nn[j] > 1 {
+					ans += (nn[j] - 1) * int(math.Pow10(j-1))
+				}
+				continue
+			}
+			if nn[j] > 0 {
+				ans += nn[j] * int(math.Pow10(j-1))
+			}
+		}
+	}
+	return ans
+}
---
+++ Interview/17_1_50/6/readme.md
@@ -0,0 +1,9 @@
+## [面试题 17.06. 2出现的次数](https://leetcode-cn.com/problems/number-of-2s-in-range-lcci/)
+
+### 说明
+编写一个方法，计算从 0 到 n (含 n) 中数字 2 出现的次数。
+
+### 实例
+#### 1
+输入: 25
+输出: 9
