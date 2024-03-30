794ec65e5ee9f0ec17cf381bb3f1bfd2c792ea8f
waited <mywaited@163.com>
2024-03-30 20:14:12 +0800

    add 算法题: 2952

---
+++ 2501_3000/2951_3000/2952/one/minimum-number-of-coins-to-be-added.go
@@ -0,0 +1,25 @@
+package one
+
+import (
+	"sort"
+"fmt"
+)
+
+func minimumAddedCoins(coins []int, target int) int {
+    sort.Ints(coins)
+    coinIndex := 0
+    sum := 0
+    addNum := 0
+    for sum < target {
+        if coinIndex < len(coins) {
+            if coins[coinIndex] <= sum || sum+1 == coins[coinIndex] {
+                sum += coins[coinIndex]
+                coinIndex++
+                continue
+            }
+        }
+        addNum++
+        sum += sum+1
+    }
+    return addNum
+}
