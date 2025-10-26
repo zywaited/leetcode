42dbf0229efba22987701c2cb625060a3fad530a
waited <mywaited@163.com>
2025-10-26 17:57:54 +0800

    add: 3346. 执行操作后元素的最高频率 I (op-v2)

--- 3001_3500/3301_3350/3346/one/max_frequency.go
+++ 3001_3500/3301_3350/3346/one/max_frequency.go
@@ -23,3 +23,36 @@ func maxFrequency(nums []int, k int, numOperations int) int {
 	}
 	return answer
 }
+
+func maxFrequencyV2(nums []int, k int, numOperations int) int {
+	sort.Ints(nums)
+	numCount := make(map[int]int, len(nums))
+	for _, num := range nums {
+		numCount[num]++
+	}
+	leftIndex := -1
+	rightIndex := 0
+	var answer int
+	for num := nums[0]; num <= nums[len(nums)-1]; num++ {
+		for ; num-nums[leftIndex+1] > k; leftIndex++ {
+		}
+		for ; rightIndex < len(nums) && nums[rightIndex]-num <= k; rightIndex++ {
+		}
+		answer = max(answer, min(rightIndex-leftIndex-1, numOperations+numCount[num]))
+	}
+	return answer
+}
+
+func min(f, s int) int {
+	if f <= s {
+		return f
+	}
+	return s
+}
+
+func max(f, s int) int {
+	if f >= s {
+		return f
+	}
+	return s
+}
